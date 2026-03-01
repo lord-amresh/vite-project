263aa7c32b66023d3653d24490200dcc574c049a
lord-amresh <amresh2006@gmail.com>
2026-03-01 20:18:45 +0545

    our 1st App component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
 
+function Title() {
+  return (
+    <h1>I am the Title.</h1>
+  )
+}
+
 function App() {
-  return   <button>Hello it's Amresh World!</button>
+  return  (
+    <div>
+      <h1>This is my Aoo Component.</h1>
+      <p>Inside App Component we have:</p>
+      <Title/>
+    </div>
+  );
 }
 
 export default App;
