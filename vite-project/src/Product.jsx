2d3fc2a9038d3f557fa3c326a951b6fdc004cdbb
lord-amresh <amresh2006@gmail.com>
2026-03-02 09:35:13 +0545

    01. React Props

--- vite-project/src/Product.jsx
+++ vite-project/src/Product.jsx
@@ -1,10 +1,10 @@
 import "./Product.css";
 
-function Product() {
+function Product({title, Price=1}) {
     return(
         <div className="Product">
-            <h3>Product Title</h3>
-            <h5>Product description.</h5>
+            <h3>{title}</h3>
+            <h5>Price : {Price} </h5>
         </div>
     );
 }
--- vite-project/src/ProductTab.jsx
+++ vite-project/src/ProductTab.jsx
@@ -4,9 +4,9 @@ import Product from "./Product.jsx";
 function ProductTab() {
     return (
     <>
-      <Product/>
-      <Product/>
-      <Product/>
+      <Product title ="Phone" Price= {30000}  />
+      <Product title = "Laptop" Price= {40000} />
+      <Product title= "Pen"  />
     </>
     );
 }
