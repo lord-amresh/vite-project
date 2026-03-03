6188481707ec424ee386404554c71842e855e97d
lord-amresh <amresh2006@gmail.com>
2026-03-03 08:53:21 +0545

    Conditionals ternary ? and &&

--- vite-project/src/Product.jsx
+++ vite-project/src/Product.jsx
@@ -1,14 +1,15 @@
 import "./Product.css";
 
 function Product({title, Price, features}) {
-    //const list = features.map((feature) => <li>{feature}</li>);
-    return(
+        return(
         <div className="Product">
             <h3>{title}</h3>
             <h5>Price : {Price} </h5>
-            <p>{features.map((feature) => <li>{feature}</li>)}</p>
+            {Price > 30000 && <p>Discount of 5%</p> }
         </div>
     );
+    
+    
 }
 
 export default Product;
--- vite-project/src/ProductTab.jsx
+++ vite-project/src/ProductTab.jsx
@@ -5,9 +5,9 @@ function ProductTab() {
   let options = ["Hi-tech", "Durable", "Fast" ]
     return (
     <>
-      <Product title ="Phone" Price= {30000}  features= {options} />
-      {/* <Product title = "Laptop" Price= {40000} />
-      <Product title= "Pen"  /> */}
+      <Product title ="Phone" Price= {30000} />
+      <Product title = "Laptop" Price= {40000} />
+      <Product title= "Pen" price= {1} />
     </>
     );
 }
