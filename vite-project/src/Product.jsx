8f33dd3ac5e516dd8698880fc0aae0b04d1bb94d
lord-amresh <amresh2006@gmail.com>
2026-03-03 09:03:24 +0545

    dynamic Styling

--- vite-project/src/Product.jsx
+++ vite-project/src/Product.jsx
@@ -1,8 +1,11 @@
 import "./Product.css";
 
 function Product({title, Price, features}) {
-        return(
-        <div className="Product">
+    let isDiscount = Price > 30000;
+    let styles = {backgroundColor: isDiscount ? "Turquoise" : ""} ;   
+    
+    return(
+        <div className="Product" style= {styles} >
             <h3>{title}</h3>
             <h5>Price : {Price} </h5>
             {Price > 30000 && <p>Discount of 5%</p> }
