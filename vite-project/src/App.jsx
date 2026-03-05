db91de0f1e00ecc4ba1b303cdaa249450d6e3cc3
lord-amresh <amresh2006@gmail.com>
2026-03-05 13:58:50 +0545

    finished upto hooks

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,8 +5,8 @@ import Button from "./Button.jsx";
 function App() {
   return  (
     <>
-      {/* <h2>Blockbuster Deals | Shop Now</h2>
-      <ProductTab/> */}
+      <h2>Blockbuster Deals | Shop Now</h2>
+      <ProductTab/>
       <Button/>
     </>
   );
